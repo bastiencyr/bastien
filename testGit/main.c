67abdf6653b5401d11ebac0a6de2c43fb8350f8f
CORIAT BASTIEN <bastien.coriat@etu.unistra.fr>
2020-04-19 15:14:02 +0200

    testGit

---
+++ testGit/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: bastien
+ *
+ * Created on 19 avril 2020, 15:13
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+	int a;
+	return (EXIT_SUCCESS);
+}
+
